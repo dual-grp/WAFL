057ef0fa00a9cbac1b33bf1203e519641830e6ab
CharlieDinh <thecanhdinh92@gmail.com>
2021-07-12 17:54:34 +1000

    create FedRob

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ import importlib
 import random
 import os
 from FLAlgorithms.servers.serveravg import FedAvg
-from FLAlgorithms.servers.serverpFedMe import pFedMe
-from FLAlgorithms.servers.serverperavg import PerAvg
-from FLAlgorithms.servers.serverFedU import FedU
-from FLAlgorithms.servers.serverlocal import FedLocal
-from FLAlgorithms.servers.serverglobal import FedGlobal
+from FLAlgorithms.servers.serverrobF import FedRob
 from utils.model_utils import read_data
 from FLAlgorithms.trainmodel.models import *
 from utils.plot_utils import *
@@ -74,6 +70,12 @@ def main(experiment, dataset, algorithm, model, batch_size, learning_rate, beta,
             if(commet):
                 experiment.set_name(dataset + "_" + algorithm + "_" + model[1] + "_" + str(batch_size) + "_" + str(learning_rate) + "_" + str(num_glob_iters) + "_"+ str(local_epochs) + "_"+ str(numusers))
             server = FedAvg(experiment, device, data, algorithm, model, batch_size, learning_rate, beta, L_k, num_glob_iters, local_epochs, optimizer, numusers, i, cutoff)
+        
+        elif(algorithm == "FedRob"):
+            if(commet):
+                experiment.set_name(dataset + "_" + algorithm + "_" + model[1] + "_" + str(batch_size) + "_" + str(learning_rate) + "_" + str(num_glob_iters) + "_"+ str(local_epochs) + "_"+ str(numusers))
+            server = FedRob(experiment, device, data, algorithm, model, batch_size, learning_rate, beta, L_k, num_glob_iters, local_epochs, optimizer, numusers, i, cutoff)
+
         else:
             print("Algorithm is invalid")
             return
