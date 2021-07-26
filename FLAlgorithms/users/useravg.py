673aed4506fc1efbe8b2d7dbb9a1a9084ffd35fd
CharlieDinh <thecanhdinh92@gmail.com>
2021-07-26 16:03:28 +1000

    update loading data for synthetic, mnist

--- FLAlgorithms/users/useravg.py
+++ FLAlgorithms/users/useravg.py
@@ -45,5 +45,4 @@ class UserAVG(User):
             loss = self.loss(output, y)
             loss.backward()
             self.optimizer.step()
-            self.clone_model_paramenter(self.model.parameters(), self.local_model)
         return LOSS
