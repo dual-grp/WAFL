56e0af49f39064e29b7bc30d866131464b2a4306
CharlieDinh <thecanhdinh92@gmail.com>
2021-07-28 12:39:56 +1000

    update useravg

--- FLAlgorithms/users/useravg.py
+++ FLAlgorithms/users/useravg.py
@@ -12,37 +12,36 @@ class UserAVG(User):
     def __init__(self, device, numeric_id, train_data, test_data, model, batch_size, learning_rate, beta, L_k, local_epochs):
         super().__init__(device, numeric_id, train_data, test_data, model[0], batch_size, learning_rate, beta, L_k, local_epochs)
 
-        #if(model[1] == "Mclr_CrossEntropy"):
-        self.loss = nn.CrossEntropyLoss()
-        #else:
-        #    self.loss = nn.NLLLoss()
+        if(model[1] == "Mclr_CrossEntropy"):
+            self.loss = nn.CrossEntropyLoss()
+        else:
+            self.loss = nn.NLLLoss()
 
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate)
-
-    # def train(self, epochs):
-    #     LOSS = 0
-    #     self.model.train()
-    #     for epoch in range(1, self.local_epochs + 1):
-    #         for X,y in self.trainloader:
-    #             X, y = X.to(self.device), y.long().to(self.device)#self.get_next_train_batch()
-    #             self.optimizer.zero_grad()
-    #             output = self.model(X)
-    #             loss = self.loss(output, y)
-    #             loss.backward()
-    #             self.optimizer.step()
-    #             LOSS += loss
-    #     self.clone_model_paramenter(self.model.parameters(), self.local_model)
-    #     return LOSS
 
     def train(self, epochs):
         LOSS = 0
         self.model.train()
         for epoch in range(1, self.local_epochs + 1):
-            self.model.train()
-            X, y = self.get_next_train_batch()
-            self.optimizer.zero_grad()
-            output = self.model(X)
-            loss = self.loss(output, y)
-            loss.backward()
-            self.optimizer.step()
+            for X,y in self.trainloader:
+                X, y = X.to(self.device), y.long().to(self.device)#self.get_next_train_batch()
+                self.optimizer.zero_grad()
+                output = self.model(X)
+                loss = self.loss(output, y)
+                loss.backward()
+                self.optimizer.step()
+                LOSS += loss
         return LOSS
+
+    # def train(self, epochs):
+    #     LOSS = 0
+    #     self.model.train()
+    #     for epoch in range(1, self.local_epochs + 1):
+    #         self.model.train()
+    #         X, y = self.get_next_train_batch()
+    #         self.optimizer.zero_grad()
+    #         output = self.model(X)
+    #         loss = self.loss(output, y)
+    #         loss.backward()
+    #         self.optimizer.step()
+    #     return LOSS
