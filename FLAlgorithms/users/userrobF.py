941985f2728f7dd0cf1ada51c5c30107fca723f5
CharlieDinh <thecanhdinh92@gmail.com>
2021-07-29 10:21:20 +1000

    update user fedrob

--- FLAlgorithms/users/userrobF.py
+++ FLAlgorithms/users/userrobF.py
@@ -22,42 +22,45 @@ class UserRobF(User):
 
     def train(self, epochs):
         self.model.train()
-        self.local_epochs_adversal = 20
-        self.local_iteration = 1
-
-        for i in range(1, self.local_iteration + 1):
-            X, y = self.get_next_train_batch()
-            X.requires_grad_(True)
-            output = self.model(X)
-            loss = self.loss(output, y)
-            if X.grad is not None:
-                X.grad.data.zero_()
-            gradX = torch.autograd.grad(self.mu*loss, X,retain_graph=True)
-            #X.requires_grad_(False)
-            X_adv = X + gradX[0] 
-            X_adv.requires_grad_(True)
-
-            # Step1 : Finding adversarial sample
-            for epoch in range(1, self.local_epochs_adversal + 1):
-                output = self.model(X_adv)
+        self.local_epochs_adversal = 10
+        #self.local_iteration = 1
+        for local_epoch in range(1, self.local_epochs + 1):
+            for X ,y in self.trainloader:
+                #X, y = self.get_next_train_batch()
+                X, y = X.to(self.device), y.long().to(self.device)
+                
+                X.requires_grad_(True)
+                output = self.model(X)
                 loss = self.loss(output, y)
-                if X_adv.grad is not None:
-                    X_adv.grad.data.zero_()
-                gradX_avd1 = torch.autograd.grad(loss, X_adv,retain_graph=True)
-                gradX_avd2 = torch.autograd.grad(0.5*self.mu*torch.norm(X_adv-X)**2,X_adv,retain_graph=True)
-                gradX_avd = gradX_avd2[0] - gradX_avd1[0]
-                X_adv = X_adv - 1./np.sqrt(epoch+2) * gradX_avd
-                #X_adv = X_adv - self.learning_rate * gradX_avd
-                #print("grad different", torch.norm(- 1./np.sqrt(epoch+2) * gradX_avd))
-                #print("---different----",torch.norm(X_adv-X))
-            #X_adv.requires_grad_(False)
+                if X.grad is not None:
+                    X.grad.data.zero_()
+                gradX = torch.autograd.grad(self.mu*loss, X, retain_graph=True)
 
-            # Step2 : Update local model using adversarial sample
-            for epoch in range(1, self.local_epochs + 1):
+                # Step1 : Finding adversarial sample
+                X.requires_grad_(False)
+                X_adv = X - gradX[0]
+                for epoch in range(1, self.local_epochs_adversal + 1):
+                    X_adv.requires_grad_(True)
+                    output = self.model(X_adv)
+                    loss = self.loss(output, y)
+                    if X_adv.grad is not None:
+                        X_adv.grad.data.zero_()
+                    gradX_avd1 = torch.autograd.grad(loss, X_adv,retain_graph=True)
+                    # using norm L1 or L2
+                    #gradX_avd2 = torch.autograd.grad(0.5*self.mu*torch.norm(X_adv-X)**2,X_adv,retain_graph=True)
+                    gradX_avd2 = torch.autograd.grad(self.mu*torch.norm(X_adv-X),X_adv,retain_graph=True)
+                    gradX_avd = gradX_avd2[0] - gradX_avd1[0]
+                    X_adv = X_adv - 1./np.sqrt(epoch+2) * gradX_avd
+
+                    #X_adv = X_adv - self.learning_rate * gradX_avd
+                    #print("grad different", torch.norm(gradX_avd))
+                    #print("---different----",torch.norm(X_adv-X))
+                #X_adv.requires_grad_(False)
+
+                # Step2 : Update local model using adversarial sample
                 self.optimizer.zero_grad()
                 output = self.model(X_adv)
                 loss = self.loss(output, y)
                 loss.backward(retain_graph=True)
                 self.optimizer.step()
-                #self.clone_model_paramenter(self.model.parameters(), self.local_model)
         return 
